93ac509e31f3fee52c09ef100fda1ae4add172cc
sunnyya <sun2547@naver.com>
2021-09-15 11:13:02 +0900

    Category 모델 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from .models import Post,Category
 
 
-from .models import Post
+
+
 
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
