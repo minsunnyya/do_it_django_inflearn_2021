31f89bedd582fb99deeef4bc6ebf6238d63218a3
sunnyya <sun2547@naver.com>
2021-09-25 15:30:12 +0900

    댓글 작성 폼 구현하기-로그인 안한 상태

--- blog/tests.py
+++ blog/tests.py
@@ -187,6 +187,19 @@ class TestView(TestCase):
         self.assertIn(self.comment_001.author.username,comment_001_area.text)
         self.assertIn(self.comment_001.content, comment_001_area.text)
 
+    def test_comment_form(self):
+        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(self.post_001.comment_set.count(), 1)
+
+        #로그인 하지 않은 상태
+        response = self.client.get(self.post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        comment_area = soup.find('div', id='comment-area')
+        self.assertIn('Log in and leave a comment', comment_area.text)
+        self.assertFalse(comment_area.find('form', id='comment-form'))
+
     def test_category_page(self):
         response = self.client.get(self.category_programming.get_absolute_url())
         self.assertEqual(response.status_code, 200)
